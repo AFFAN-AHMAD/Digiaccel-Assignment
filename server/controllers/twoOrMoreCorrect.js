60233ad15e43553575011c5df8641d3370c783a0
Affan Ahmad <96197969+AFFAN-AHMAD@users.noreply.github.com>
2022-11-03 10:45:41 +0530

    added:comments to the routes

---
+++ server/controllers/twoOrMoreCorrect.js
@@ -0,0 +1,5 @@
+const twoOrMoreCorrect = async(rec,res)=>{
+
+}
+
+module.exports = twoOrMoreCorrect
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -6,13 +6,22 @@ const login = require("../controllers/login");
 const protect = require("../middlewares/protect");
 const createQuiz = require("../controllers/createQuiz");
 const verifyUserRole = require("../controllers/verifyUserRole");
-const addQuestion = require("../controllers/addQuestion")
+const addQuestion = require("../controllers/addQuestion");
+const twoOrMoreCorrect = require("../controllers/twoOrMoreCorrect");
+
 userRoute.post("/signup", signup);
 userRoute.post("/login", login);
 
+// for verifying the role of the person logged in
 userRoute.get("/verifyUserRole", protect, verifyUserRole);
 
+// for creating a quiz
 userRoute.get("/createQuiz", protect, createQuiz);
+
+// for adding a question having only one answer correct
 userRoute.get("/addQuestion", protect, addQuestion);
+
+// for adding a question having more than one answer correct
+userRoute.get("/twoOrMore", protect, twoOrMoreCorrect);
 
 module.exports = userRoute;
