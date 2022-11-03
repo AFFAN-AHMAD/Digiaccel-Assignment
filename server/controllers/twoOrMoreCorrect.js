d7b0b09c56e8a401f2ebcb084adcf544a51f9e51
Affan Ahmad <96197969+AFFAN-AHMAD@users.noreply.github.com>
2022-11-03 11:31:49 +0530

    created:two or more answers correct controller

--- server/controllers/twoOrMoreCorrect.js
+++ server/controllers/twoOrMoreCorrect.js
@@ -1,5 +1,18 @@
-const twoOrMoreCorrect = async(rec,res)=>{
+const TwoOrMoreModel = require("../models/TwoOrMore.model");
 
-}
+const twoOrMoreCorrect = async (rec, res) => {
+  try {
+    let { question, correctAnswer, allAnswers } = req.body;
+    let quest = await TwoOrMoreModel({
+      question,
+      allAnswers,
+      correctAnswer,
+    });
+    quest.save();
+    return res.status(200).send("question is created sucessfully");
+  } catch (err) {
+    return res.send(err);
+  }
+};
 
-module.exports = twoOrMoreCorrect
+module.exports = twoOrMoreCorrect;
