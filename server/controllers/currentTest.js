f6f16d851403ac0c3a6f14a7a64f6ef1bf8e03ca
Affan Ahmad <96197969+AFFAN-AHMAD@users.noreply.github.com>
2022-11-07 23:52:11 +0530

    updated:current test

--- server/controllers/currentTest.js
+++ server/controllers/currentTest.js
@@ -1,3 +1,17 @@
-const currentTest = async (req, res) => {};
+const QuizModel = require("../models/QuizType1.model");
+
+const currentTest = async (req, res) => {
+  try {
+    let { id } = req.headers;
+    console.log(id);
+    let test = await QuizModel.findOne({ _id: id });
+    console.log("test", test);
+    return res.send(test);
+  } catch (err) {
+    return res.send(err);
+  } finally {
+    return;
+  }
+};
 
 module.exports = currentTest;
